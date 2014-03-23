809b48ff3398c730c4e99c4454c948584320ddc2
dipunm <dipunm@gmail.com>
2014-03-23 11:41:37 +0000

    Enabled note persistence via local file

--- TimeLogger.Main/Views/Windows/LoggerWindow.xaml.cs
+++ TimeLogger.Main/Views/Windows/LoggerWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Windows;
 using MVVM.Extensions;
 using TimeLogger.Wpf.ViewModels;
+using System.IO;
 
 namespace TimeLogger.Wpf.Views.Windows
 {
@@ -13,7 +14,27 @@ namespace TimeLogger.Wpf.Views.Windows
         public LoggerWindow()
         {
             InitializeComponent();
+            this.Closing += (s, e) => SaveToFile();
+            LoadFromFile();
         }
+
+        private void SaveToFile()
+        {
+            var content = NotesBox.Text;
+            File.WriteAllText(filename, content, System.Text.Encoding.UTF8);
+        }
+
+        private const string filename = "notes.txt";
+
+        private void LoadFromFile()
+        {
+            if (!File.Exists(filename))
+                return;
+
+            string content = File.ReadAllText(filename, System.Text.Encoding.UTF8);
+            NotesBox.Text = content;
+        }
+
 
         public void SetViewModel(LoggerViewModel model)
         {
